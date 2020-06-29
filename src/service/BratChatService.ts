d1a5cd35ac810a95db3c7a07e77825c6fd5c7c26
Niklas Schlögel <niklas.schloegel@student.hs-rm.de>
2020-06-29 17:07:32 +0200

    created BratChatService.ts for communication with server for "Community Chat"

---
+++ src/service/BratChatService.ts
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import CompositionApi, {ref, computed} from '@vue/composition-api'
+import {Client, Message} from '@stomp/stompjs'
+
+Vue.use(CompositionApi)
+
+const lines = ref(new Array<string>())
+const isConnected = ref(false)
+
+const wsurl = "ws://localhost:9090/stompbroker"
+const SEND = "/topic/bratchat/toserver"
+const RCV = "/topic/bratchat/fromserver"
+
+let stompclient: Client
+
+function startChat() {
+    if (!stompclient) {
+        stompclient = new Client({brokerURL: wsurl})
+        stompclient.onConnect = (frame) => {
+            stompclient.subscribe(RCV, (message) => {
+                let delcount = lines.value.length < 20 ? 0 : 1
+                lines.value.splice(0, delcount, message.body)
+            })
+        }
+    }
+    stompclient.activate()
+}
+
+function send(nachricht: string) {
+    startChat()
+    try {
+        stompclient.publish({destination: SEND, headers: {}, body: nachricht})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export default function useBratChatService() {
+    return {
+        lines: computed(() => lines),
+        isConnected: computed(() => isConnected),
+        send,
+        startChat
+    }
+}
