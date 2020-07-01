ff9d0559aa5f5c2d4ac16a2f716c774f214ee8bd
Niklas Schlögel <niklas.schloegel@student.hs-rm.de>
2020-07-01 18:31:43 +0200

    integrated router

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,54 @@
 <template>
-  <section id="app">
-    <div class="hero is-primary">
-      <div class="hero-body container">
-        <h1 class="title">Bratenbank</h1>
-      </div>
-    </div>
-    <div class="container">
-      <div class="block">
-        <nav class="navbar">
-          <div class="navbar-menu is-active">
-            <a href="/home" class="navbar-item">Bratenliste</a>
-            <a href="/welt" class="navbar-item">Bratenwelt</a>
-          </div>
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <a href="/logout" class="navbar-item">Logout</a>
+    <section id="app">
+        <div class="hero is-primary">
+            <div class="hero-body container">
+                <h1 class="title">Bratenbank</h1>
             </div>
-          </div>
-        </nav>
-      </div>
-
-      <div class="columns">
-        <div class="column is-three-quarters">
-          <div class="block">
-            <!--
-            <BratenListeView/>
-            -->
-
-            <BratenChatView/>
-          </div>
         </div>
-        <div class="column">
-          <VegetarizitaetsRechner />
-          <BratChat/>
+        <div class="container">
+            <div class="block">
+                <nav class="navbar">
+                    <div class="navbar-menu is-active">
+                        <router-link to="/liste" class="navbar-item">Bratenliste</router-link>
+                        <router-link to="/chat" class="navbar-item">Bratenchat</router-link>
+                    </div>
+                    <!--
+                    <div class="navbar-end">
+                      <div class="navbar-item">
+                        <a href="/logout" class="navbar-item">Logout</a>
+                      </div>
+                    </div>
+                      -->
+                </nav>
+            </div>
+
+            <div class="columns">
+                <div class="column is-three-quarters">
+                    <div class="block">
+                        <router-view/>
+                    </div>
+                </div>
+                <div class="column">
+                    <VegetarizitaetsRechner/>
+                    <BratChat/>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import VegetarizitaetsRechner from "@/components/VegetarizitaetsRechner.vue";
-import BratChat from "@/components/BratChat.vue";
-//import BratenListeView from "@/views/BratenListeView.vue";
-import BratenChatView from "@/views/BratenChatView.vue";
+    import Vue from "vue";
+    import VegetarizitaetsRechner from "@/components/VegetarizitaetsRechner.vue";
+    import BratChat from "@/components/BratChat.vue";
 
-export default Vue.extend({
-  name: "App",
-  components: {
-    VegetarizitaetsRechner,
-    BratenChatView,
-    BratChat
-  }
-});
+    export default Vue.extend({
+        name: "App",
+        components: {
+            VegetarizitaetsRechner,
+            BratChat
+        }
+    });
 </script>
 
 <style>
